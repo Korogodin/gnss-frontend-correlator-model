9118bf90db3013eca2211ea9655acce706a6f944
korogodin_iv@mail.ru <korogodin_iv@mail.ru@fac677cb-4689-81b1-f120-911310e7d2cd>
2011-10-04 11:41:26 +0000

    minor bugfix

---
+++ lists/additional_paths.m
@@ -0,0 +1,10 @@
+addpath( [pwd '/outside_callback_func'] );
+addpath( [pwd '/outside_callback_func/out_reg'] );
+addpath( [pwd '/outside_callback_func/out_ADC'] );
+addpath( [pwd '/outside_callback_func/out_YP4'] );
+addpath( [pwd '/outside_callback_func/out_RG'] );
+addpath( [pwd '/outside_callback_func/out_SinCos'] );
+addpath( [pwd '/outside_callback_func/out_Clock'] );
+addpath( [pwd '/outside_callback_func/open_axes_buttons'] );
+
+addpath( [pwd '/outside_func'] );
