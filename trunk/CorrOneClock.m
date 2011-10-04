ab9e7820d2e099ceeadb36e20617a6ea0b3e8056
korogodin_iv@mail.ru <korogodin_iv@mail.ru@fac677cb-4689-81b1-f120-911310e7d2cd>
2011-10-04 11:31:33 +0000

    Забыл удалить

--- trunk/CorrOneClock.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = CorrOneClock( input_args )
-%CORRONECLOCK Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- trunk/ministtem.m
+++
@@ -1,9 +0,0 @@
-function h = ministtem(hAx, t, y)
-
-h = stem(hAx, t, y);
-        set(hAx, 'XTick', []);
-        set(hAx, 'YTick', []);
-        set(h,'MarkerSize', 3)
-        
-end
-
